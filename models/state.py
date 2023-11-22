053373c851d27c5f7e892c43b9a38a3653db5c54
omnia abd elaleem mohamed <120645154+OMNIA93@users.noreply.github.com>
2023-11-22 23:38:23 +0200

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states' 
+  #  name = ""
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+engine = create_engine('sqlite:///your_database.db')
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
