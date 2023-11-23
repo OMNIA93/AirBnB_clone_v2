348e7aedcd1168f668316c28de78bb28ac550bf2
omnia abd elaleem mohamed <120645154+OMNIA93@users.noreply.github.com>
2023-11-23 03:19:16 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -62,6 +62,3 @@ class FileStorage:
             if key in FileStorage.__objects:
                 del FileStorage.__objects[key]
                 self.save()
-
-    def close(self):
-        self.reload()
